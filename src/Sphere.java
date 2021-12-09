5851f438b5a65f02625804bc9b94d30f2c066d70
Aparna Misar <aparnamisar28@gmail.com>
2021-12-09 19:05:03 +0530

    Implement Area and Diameter of the shape Sphere

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,16 +1,25 @@
 public class Sphere {
     double radius;
 
-    public Sphere(double r){
+    public Sphere(double r) {
         this.radius = r;
     }
 
-    public double area(){
-        return(4 * Math.PI *radius * radius);
+    public double area() {
+        try {
+            return (4 * Math.PI * radius * radius);
+        } catch (ArithmeticException c) {
+            c.getMessage();
+            return (0.0);
+        }
     }
 
-    public double diameter(){
-        return(2 * radius);
+    public double diameter() {
+        try {
+            return (2 * radius);
+        } catch (ArithmeticException c1) {
+            c1.getMessage();
+            return (0.0);
+        }
     }
-
 }
